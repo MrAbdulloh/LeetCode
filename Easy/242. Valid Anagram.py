74d4a937d35cc386f8646f73fe715f28ac6a9566
roots <akramovabdullo2002@gmail.com>
2024-02-02 10:36:21 +0500

    242. Valid Anagram

---
+++ Easy/242. Valid Anagram.py
@@ -0,0 +1,47 @@
+"""
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+"""
+
+
+class Solution(object):
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) == len(t) and sorted(s) == sorted(t):
+            return True
+        else:
+            return False
+
+
+s = Solution()
+print(s.isAnagram("car", "car"))
+
+# 2 method
+from collections import Counter
+
+
+def isAnagram(s: str, t: str) -> bool:
+    return Counter(s) == Counter(t)
+
+
+print(isAnagram("ca", "car"))
