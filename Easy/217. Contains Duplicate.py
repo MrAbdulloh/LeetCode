bbb8ddcb40fac6597d117d8e0eb9f559983887e5
roots <akramovabdullo2002@gmail.com>
2024-02-02 10:12:05 +0500

    217. Contains Duplicate

---
+++ Easy/217. Contains Duplicate.py
@@ -0,0 +1,32 @@
+"""
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+"""
+from typing import List
+
+
+def containsDuplicate(nums: List[int]) -> bool:
+    list_duplicate = []
+    for num in nums:
+        if num in list_duplicate:
+            return True
+        list_duplicate.append(num)
+    return False
+
+
+nums = [1, 2, 3, 4]
+print(containsDuplicate(nums))
