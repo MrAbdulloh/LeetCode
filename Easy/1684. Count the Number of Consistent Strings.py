6f7b210fbba601f6ffe0e117b2d4e89fb8b206ce
roots <akramovabdullo2002@gmail.com>
2024-02-12 18:19:49 +0500

    1684. Count the Number of Consistent Strings

---
+++ Easy/1684. Count the Number of Consistent Strings.py
@@ -0,0 +1,39 @@
+"""
+You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+
+Return the number of consistent strings in the array words.
+
+
+
+Example 1:
+
+Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+Output: 2
+Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
+Example 2:
+
+Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+Output: 7
+Explanation: All strings are consistent.
+Example 3:
+
+Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+Output: 4
+Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
+"""
+
+
+def countConsistentStrings(allowed: str, words):
+    count = 0
+    for word in words:
+        count += 1
+        for letter in word:
+            if letter not in allowed:
+                count -= 1
+                break
+    return count
+
+
+allowed = "abc"
+words = ["a", "b", "c", "ab", "ac", "bc", "abc"]
+print(countConsistentStrings(allowed, words))
