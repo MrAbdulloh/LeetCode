13c96878520e3d9459f37d29440da06b9cf03892
roots <akramovabdullo2002@gmail.com>
2024-02-12 17:52:05 +0500

    709. To Lower Case

---
+++ Easy/709. To Lower Case.py
@@ -0,0 +1,23 @@
+"""
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+
+"""
+
+
+def toLowerCase(s: str) -> str:
+    return s.lower()
