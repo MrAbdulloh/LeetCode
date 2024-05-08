077a73ee1e4ba90293cfe902808092746fc4deff
MrAbdulloh <akramovabdullo2002@gmail.com>
2024-05-08 06:50:04 +0500

    1047. Remove All Adjacent Duplicates In String

---
+++ Easy/Filter/Stack/1047. Remove All Adjacent Duplicates In String.py
@@ -0,0 +1,12 @@
+def removeDuplicates(s: str) -> str:
+    stack = []
+    for char in s:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+    return ''.join(stack)
+
+
+s = "abbaca"
+print(removeDuplicates(s))
