12c223f2456dc11f1cdb0d3bf5a8604b9a18a8e2
roots <akramovabdullo2002@gmail.com>
2024-07-01 17:35:45 +0500

    1550. Three Consecutive Odds

---
+++ Easy/Filter/Array/1550. Three Consecutive Odds.py
@@ -0,0 +1,23 @@
+def threeConsecutiveOdds(arr: list[int]) -> bool:
+
+    ## 2 method
+
+    # size = len(arr)
+    # for i in range(size - 2):
+    #     if arr[i] % 2 != 0 and arr[i + 1] % 2 != 0 and arr[i + 2] % 2 != 0:
+    #         return True
+    # return False
+
+    ## 2 method
+
+    odds = 0
+    for num in arr:
+        if num % 2 == 1:
+            odds += 1
+            if odds == 3:
+                return True
+        odds = 0
+    return False
+
+arr = [1, 2, 3, 4, 5, 7, 9, 11]
+print(threeConsecutiveOdds(arr))
