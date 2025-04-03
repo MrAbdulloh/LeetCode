474c66cc8feffb66f7b15e1a41cf0141f79292dd
MrAbdulloh <akramovabdullo2002@gmail.com>
2025-04-03 11:21:49 +0500

    2874. Maximum Value of an Ordered Triplet II

---
+++ Python/Medium/Array/2874__Maximum_Value_of_an_Ordered_Triplet_II.py
@@ -0,0 +1,26 @@
+def maximumTripletValue(nums: list[int]) -> int:
+    n = len(nums)
+    if n < 3:
+        return 0
+
+    left_max = [0] * n
+    left_max[0] = nums[0]
+    for i in range(1, n):
+        left_max[i] = max(left_max[i - 1], nums[i])
+
+    right_max = [0] * n
+    right_max[-1] = nums[-1]
+    for i in range(n - 2, -1, -1):
+        right_max[i] = max(right_max[i + 1], nums[i])
+
+    ans = 0
+    for i in range(1, n - 1):
+        left = left_max[i - 1]
+        right = right_max[i + 1]
+        ans = max(ans, (left - nums[i]) * right)
+
+    return max(ans, 0)
+
+
+nums = [1, 10, 3, 4, 19]
+print(maximumTripletValue(nums))
