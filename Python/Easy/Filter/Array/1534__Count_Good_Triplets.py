a320137dc7a0fa16a47033c140945e7dc0634ca1
MrAbdulloh <akramovabdullo2002@gmail.com>
2025-04-14 09:30:44 +0500

    1534. Count Good Triplets

---
+++ Python/Easy/Filter/Array/1534__Count_Good_Triplets.py
@@ -0,0 +1,19 @@
+def countGoodTriplets(arr: list[int], a: int, b: int, c: int) -> int:
+    count = 0
+    n = len(arr)
+    for i in range(n):
+        for j in range(i + 1, n):
+            for k in range(j + 1, n):
+                if (abs(arr[i] - arr[j]) <= a and
+                        abs(arr[j] - arr[k]) <= b and
+                        abs(arr[i] - arr[k]) <= c):
+                    count += 1
+    return count
+
+
+arr = [3, 0, 1, 1, 9, 7]
+a = 7
+b = 2
+c = 3
+
+print(countGoodTriplets(arr, a, b, c))
